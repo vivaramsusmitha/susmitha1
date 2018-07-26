f501ea86a55efa1a975d322cefe3cb3f1f448daf
vivaramsusmitha <41729707+vivaramsusmitha@users.noreply.github.com>
2018-07-26 16:53:18 +0530

    Create susmi.java

---
+++ begin/susmi.java
@@ -0,0 +1,30 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+	   int	i=3;
+	   if(i<3)
+	   {
+	   	System.out.println("positive");
+	   }
+	   else if (i>3)
+	   {
+	   	System.out.println("negative");
+	   	
+	   }
+	   else
+	   { 
+	   	 System.out.println("zero");
+	   	
+	   }
+		
+	}
+}
